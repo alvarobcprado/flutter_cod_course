3631c06815327ddc91bcbbb42d8dc7406ee739d8
Álvaro Prado <alvaroprado@tokenlab.com.br>
2022-10-18 08:47:27 -0400

    Refact layout with ColoredBoxColumn widget

---
+++ lib/app/common/widgets/colored_box_column.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/cupertino.dart';
+
+class ColoredBoxColumn extends StatelessWidget {
+  const ColoredBoxColumn({
+    super.key,
+    required this.boxColor,
+    required this.boxSize,
+    required this.boxPadding,
+    required this.children,
+    this.boxBorderRadius = const BorderRadius.vertical(
+      top: Radius.circular(32),
+    ),
+  });
+
+  final Color boxColor;
+  final Size boxSize;
+  final EdgeInsetsGeometry boxPadding;
+  final BorderRadius boxBorderRadius;
+  final List<Widget> children;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        color: boxColor,
+        borderRadius: boxBorderRadius,
+      ),
+      height: boxSize.height,
+      width: boxSize.width,
+      child: SingleChildScrollView(
+        padding: boxPadding,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: children,
+        ),
+      ),
+    );
+  }
+}
--- lib/app/pages/auth/landing/landing_auth_page.dart
+++ lib/app/pages/auth/landing/landing_auth_page.dart
@@ -4,6 +4,7 @@ import 'package:flutter_cod_course/app/common/routes.dart';
 import 'package:flutter_cod_course/app/common/theme/cod_colors.dart';
 import 'package:flutter_cod_course/app/common/theme/cod_typography.dart';
 import 'package:flutter_cod_course/app/common/view_utils.dart';
+import 'package:flutter_cod_course/app/common/widgets/colored_box_column.dart';
 import 'package:flutter_cod_course/app/common/widgets/colored_button.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
@@ -31,55 +32,43 @@ class LandingAuthPage extends StatelessWidget {
                 height: 200.fromHeight(context),
               ),
             ),
-            Container(
-              decoration: BoxDecoration(
-                color: colors.purple,
-                borderRadius: const BorderRadius.vertical(
-                  top: Radius.circular(32),
+            ColoredBoxColumn(
+              boxColor: colors.purple,
+              boxPadding: EdgeInsets.all(24.fromHeight(context)),
+              boxSize: Size.fromHeight(376.fromHeight(context)),
+              children: [
+                Text(
+                  'Bem vindo(a) ao Chief',
+                  style: typo.titleH1.copyWith(
+                    color: colors.beige,
+                  ),
                 ),
-              ),
-              height: 376.fromHeight(context),
-              width: double.infinity,
-              child: SingleChildScrollView(
-                padding: EdgeInsets.all(24.fromHeight(context)),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      'Bem vindo(a) ao Chief',
-                      style: typo.titleH1.copyWith(
-                        color: colors.beige,
-                      ),
-                    ),
-                    SizedBox(height: 16.fromHeight(context)),
-                    Text(
-                      'Essa é a área de membros do Chief. Caso já tenha uma conta, clique em logar, senão, clique em criar conta.',
-                      style: typo.body.copyWith(
-                        color: colors.beige,
-                      ),
-                    ),
-                    SizedBox(height: 32.fromHeight(context)),
-                    ColoredButton(
-                      backgroundColor: colors.orange,
-                      textColor: colors.beige,
-                      text: 'Login',
-                      onPressed: () {
-                        Navigator.of(context).pushNamed(CodRoutes.signin);
-                      },
-                    ),
-                    SizedBox(height: 24.fromHeight(context)),
-                    ColoredButton(
-                      backgroundColor: colors.beige,
-                      textColor: colors.purple,
-                      text: 'Criar conta',
-                      onPressed: () {
-                        Navigator.of(context).pushNamed(CodRoutes.signup);
-                      },
-                    ),
-                  ],
+                SizedBox(height: 16.fromHeight(context)),
+                Text(
+                  'Essa é a área de membros do Chief. Caso já tenha uma conta, clique em logar, senão, clique em criar conta.',
+                  style: typo.body.copyWith(
+                    color: colors.beige,
+                  ),
                 ),
-              ),
+                SizedBox(height: 32.fromHeight(context)),
+                ColoredButton(
+                  backgroundColor: colors.orange,
+                  textColor: colors.beige,
+                  text: 'Login',
+                  onPressed: () {
+                    Navigator.of(context).pushNamed(CodRoutes.signin);
+                  },
+                ),
+                SizedBox(height: 24.fromHeight(context)),
+                ColoredButton(
+                  backgroundColor: colors.beige,
+                  textColor: colors.purple,
+                  text: 'Criar conta',
+                  onPressed: () {
+                    Navigator.of(context).pushNamed(CodRoutes.signup);
+                  },
+                ),
+              ],
             ),
           ],
         ),
