f27e0a0e729ca20d29dadd54a9d2570613d39975
Álvaro Prado <alvaroprado@tokenlab.com.br>
2022-10-19 13:50:24 -0400

    Create HomeLastClassSection

---
+++ lib/app/pages/home/widgets/home_last_class_section.dart
@@ -0,0 +1,35 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_cod_course/app/common/view_utils.dart';
+import 'package:flutter_cod_course/app/common/widgets/cod_class_progress_card.dart';
+import 'package:flutter_cod_course/app/pages/home/widgets/home_section_header.dart';
+
+class HomeLastClassSection extends StatelessWidget {
+  const HomeLastClassSection({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final classDuration = Random().nextInt(60);
+    final classProgress = Random().nextInt(classDuration);
+    return Column(
+      children: [
+        HomeSectionHeader(
+          headerTitle: 'Última aula',
+          showMoreTitle: 'ver mais',
+          onTapShowMore: () {},
+        ),
+        SizedBox(height: 8.fromHeight(context)),
+        CodClassProgressCard(
+          classTitle: 'Cores',
+          classSubtitle: 'O que é RGB?',
+          classDuration: Duration(minutes: classDuration),
+          classProgress: Duration(minutes: classProgress),
+          onTap: () {},
+        ),
+      ],
+    );
+  }
+}
